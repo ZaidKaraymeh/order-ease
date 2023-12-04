49cdef2427f65340da45a60d81e3ff216119b400
Zaid <karaymehzaid@gmail.com>
2023-12-04 06:12:22 +0300

    Style layout to be mobile responsive

--- core/views.py
+++ core/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from core import models
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+
+    retaurant_categories = models.RestaurantCategory.objects.filter(is_enabled = True)
+
+    context = {
+        "categories": retaurant_categories
+    }
+
+    return render(request, "home.html", context)
 
 def search(request):
     return render(request, "home.html")
